fd8bcdd19793275dcdc698c1e337c800d22b8fc3
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-08-11 11:59:54 +1200

    add if statement for url.value

--- script.js
+++ script.js
@@ -37,4 +37,7 @@ html_string = """
 
 // Parse HTML tables from the web
 const readHtmlWeb = () => {
+  if (url.value === "") {
+    return alert("Please enter a url in the 'url' field, in the 'Parse HTML tables from the web' section.");
+  }
 }
